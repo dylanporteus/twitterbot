8c54d132a5eaf5e16ca4a5b98dd461294b98692f
Dylan Porteus <dylanporteus@aol.com>
2018-04-07 22:41:59 -0400

    add post functionality

--- index.js
+++ index.js
@@ -6,13 +6,29 @@ var config = require('./config');
 
 var T = new Twit(config);
 
+//GET
+
 var params = {
-	q: 'blah blah blah',
+	q: 'yeet',
 	count: 10
 };
 
 T.get('search/tweets', params, gotData);
 
 function gotData(err, data, response){
-	console.log(data)
+	var tweets = data.statuses;
+	for (var i = 0; i < tweets.length; i++){
+		console.log(tweets[i].text);
+	}
 };
+
+//POST
+var tweet = {
+	status: 'Do not go gentle into that good night. Rage, rage against the dying of the light.'
+}
+
+T.post('statuses/update', tweet, tweeted);
+
+function tweeted(err, data, response){
+	console.log("It worked!");
+}
