c036583a748c9ae77e1566074e39d45b390edd79
Dylan Porteus <dylanporteus@aol.com>
2018-04-08 21:46:20 -0400

    add set interval functionality for tweets

--- index.js
+++ index.js
@@ -8,27 +8,40 @@ var T = new Twit(config);
 
 //GET
 
-var params = {
-	q: 'yeet',
-	count: 10
-};
+// var params = {
+// 	q: 'yeet',
+// 	count: 10
+// };
 
-T.get('search/tweets', params, gotData);
+// T.get('search/tweets', params, gotData);
 
-function gotData(err, data, response){
-	var tweets = data.statuses;
-	for (var i = 0; i < tweets.length; i++){
-		console.log(tweets[i].text);
-	}
-};
+// function gotData(err, data, response){
+// 	var tweets = data.statuses;
+// 	for (var i = 0; i < tweets.length; i++){
+// 		console.log(tweets[i].text);
+// 	}
+// };
 
 //POST
-var tweet = {
-	status: 'Do not go gentle into that good night. Rage, rage against the dying of the light.'
-}
 
-T.post('statuses/update', tweet, tweeted);
+tweetIt();
 
-function tweeted(err, data, response){
+
+setInterval(tweetIt, 1000*60*60) //This will cause the bot to post to Twitter automatically (once per hour in this case)
+
+
+
+function tweetIt(){
+
+  var r = Math.floor(Math.random()*100);
+
+  var tweet = {
+	status: 'Here is a random number ' + r 
+  }
+
+  T.post('statuses/update', tweet, tweeted);
+
+  function tweeted(err, data, response){
 	console.log("It worked!");
+ }
 }
